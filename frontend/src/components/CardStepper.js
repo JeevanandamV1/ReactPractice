c71b3499c59232e5d983813670da93b50493b7a7
JeevanandamV1 <jeevanandam@ecosmossolutions.com>
2025-02-19 18:07:16 +0530

    complete merge in main

--- frontend/src/components/CardStepper.js
+++ frontend/src/components/CardStepper.js
@@ -2,31 +2,30 @@ import React, { useContext } from "react";
 import { multiStepContext } from "../pages/MultiStepForm";
 
 export default function CardStepper() {
-  const { cardformdata, handleSecondStepperFormChange } =
-    useContext(multiStepContext);
+  const { hookFormData, handleChange } = useContext(multiStepContext);
 
   return (
     <div>
       <h2>Enter your Card Details</h2>
       <input
-        value={cardformdata.cardName}
-        onChange={handleSecondStepperFormChange}
+        value={hookFormData.cardName}
+        onChange={handleChange}
         placeholder="Card Name"
         name="cardName"
         type="text"
         required
       />
       <input
-        value={cardformdata.cardNumber}
-        onChange={handleSecondStepperFormChange}
+        value={hookFormData.cardNumber}
+        onChange={handleChange}
         placeholder="Card Number"
         name="cardNumber"
         type="text"
         required
       />
       <input
-        value={cardformdata.cardCVV}
-        onChange={handleSecondStepperFormChange}
+        value={hookFormData.cardCVV}
+        onChange={handleChange}
         placeholder="Enter CVV"
         name="cardCVV"
         type="text"
